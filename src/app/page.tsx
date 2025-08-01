26a7bfead7f00a616f43bfc8354c3b14e6b4f05b
Thiago Kersting Puls <thiago.kersting@libercapital.com.br>
2025-08-01 19:15:49 -0300

    feat: data-table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,7 @@ export default function Page() {
   return (
     <>
       <SectionCards />
-      <div className="px-4 lg:px-6">
-        <ChartAreaInteractive />
-      </div>
+      <ChartAreaInteractive />
       <DataTable data={data} />
     </>
   );
